c3691327e8b11902a5cb7f9506426988565a2b81
Carrington-dev <crn96m@gmail.com>
2024-11-06 09:49:18 +0200

    Splitted pages

--- includes/header.php
+++ includes/header.php
@@ -14,7 +14,7 @@
 <div class="branding d-flex align-items-center">
 
   <div class="container position-relative d-flex align-items-center justify-content-end">
-    <a href="index.php" class="logo d-flex align-items-center me-auto">
+    <a href="/" class="logo d-flex align-items-center me-auto">
       <!-- <img src="assets/img/logo.png" alt=""> -->
       <!-- Uncomment the line below if you also wish to use a text logo -->
       <h1 class="sitename"><?php echo "$company"; ?></h1> 
@@ -22,13 +22,13 @@
 
     <nav id="navmenu" class="navmenu">
       <ul>
-        <li><a href="#hero" class="active">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#gallery">Portfolio</a></li>
+        <li><a href="/" class="active">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="services.php">Services</a></li>
+        <li><a href="gallery.php">Gallery</a></li>
         <!-- <li><a href="#doctors">Doctors</a></li> -->
         
-        <li><a href="#contact">Contact</a></li>
+        <li><a href="contact.php">Contact</a></li>
       </ul>
       <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
     </nav>
